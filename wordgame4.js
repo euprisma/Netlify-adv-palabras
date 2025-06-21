184e91d4f3b6eb09f77aa471969ecf1fa9542cfd
euprisma <euprisma@hotmail.com>
2025-06-21 09:33:24 +0200

    bold text

--- wordgame4.js
+++ wordgame4.js
@@ -466,7 +466,7 @@ async function create_game_ui(mode = null, player1 = null, player2 = null, diffi
         return { mode, player1, player2, prompt, input, button, output, container, difficulty };
     }
 
-    prompt.innerText = 'Ingresa 1 para **un jugador**, 2 para **dos jugadores**, o 3 para **jugador contra IA**:';
+    prompt.innerHTML = 'Ingresa 1 para <strong>un jugador</strong>, 2 para <strong>dos jugador</strong>, o 3 para <strong>jugador contra IA</strong>:';
     if (input.parentNode) input.focus();
 
     return new Promise(resolve => {
